da9bc7821ca25b1612dd48faaa7a368253ee3ced
Akirozen <saimirbaraj@gmail.com>
2021-12-20 15:53:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { ProductsProvider } from './context/products_context'
-import { FilterProvider } from './context/filter_context'
-import { CartProvider } from './context/cart_context'
-import { UserProvider } from './context/user_context'
-import { Auth0Provider } from '@auth0/auth0-react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
+// Configured Auth0
 ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
@@ -25,5 +26,5 @@ ReactDOM.render(
       </ProductsProvider>
     </UserProvider>
   </Auth0Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
